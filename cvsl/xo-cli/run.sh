c84b1f1f3f90f6ff01a70b4a07e3b4ac6518d72f
Rodrigo Azevedo <zvdrodrigo@gmail.com>
2025-07-11 17:40:05 -0300

    fix

--- cvsl/xo-cli/run.sh
+++ cvsl/xo-cli/run.sh
@@ -13,3 +13,10 @@ docker run --rm -it \
 -e SCRIPT_FILE="opnsense.sh" \
 -v $(pwd):/scripts \
 rodrigorao/xo-cli:alpine "/scripts/opnsense.sh"
+
+docker run --rm -it \
+-e XO_URL="https://${XOA_IP}" \
+-e XO_TOKEN="uF3Tzz01I4tEdPjb8vULOdKzXsq4hs3vS_mljZXx7Yc" \
+-e XO_ALLOW_UNAUTHORIZED=true \
+-v $(pwd):/scripts \
+rodrigorao/xo-cli:alpine "/scripts/opnsense.sh"
